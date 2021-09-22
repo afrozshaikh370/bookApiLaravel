cbb0b50ae04c7db541733beaeb8294d0dcbb6a45
afrozshaikh370 <afrozshaikh78901>
2021-09-22 14:18:55 +0530

    database

--- app/Http/Controllers/bookcontroller.php
+++ app/Http/Controllers/bookcontroller.php
@@ -4,14 +4,17 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\DB;
 use App\Models\booksModel;
 
 class bookcontroller extends Controller
 {
     function getBooks(){
+        $users = DB::table('books')->get();
         return response()->json([
             'success'=>true,
-            'data'=>booksModel::all()
+            
+            'data'=>$users
         ]);
         
     }
@@ -22,17 +25,22 @@ class bookcontroller extends Controller
             $books->title=$req->title;
             $books->email=$req->email;
             $books->category=$req->category;
+            if($req->hasfile("imgRes"))
+            {
                 $file=$req->file('imgRes');
                 $extention = $file->getClientOriginalExtension();
                 $filename = time().'.'.$extention;
                 $file->move('images',$filename);
                 $books->imgRes="images/".$filename;
+            }
+            if($req->hasfile("pdfRes"))
+        {
             $file=$req->file('pdfRes');
             $extention = $file->getClientOriginalExtension();
             $filename = time().'.'.$extention;
             $file->move('pdfs',$filename);
             $books->pdfRes="pdfs/".$filename;
-        
+        }
             $books->save();
             return view("/uploadbooks");
 
