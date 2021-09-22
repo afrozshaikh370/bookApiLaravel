8f8afe9ef2615c0576aaad896c7d8f40392a83d0
afrozshaikh370 <afrozshaikh78901>
2021-09-22 14:05:02 +0530

    database

--- app/Http/Controllers/bookcontroller.php
+++ app/Http/Controllers/bookcontroller.php
@@ -22,22 +22,17 @@ class bookcontroller extends Controller
             $books->title=$req->title;
             $books->email=$req->email;
             $books->category=$req->category;
-            if($req->hasfile("imgRes"))
-            {
                 $file=$req->file('imgRes');
                 $extention = $file->getClientOriginalExtension();
                 $filename = time().'.'.$extention;
                 $file->move('images',$filename);
                 $books->imgRes="images/".$filename;
-            }
-            if($req->hasfile("pdfRes"))
-        {
             $file=$req->file('pdfRes');
             $extention = $file->getClientOriginalExtension();
             $filename = time().'.'.$extention;
             $file->move('pdfs',$filename);
             $books->pdfRes="pdfs/".$filename;
-        }
+        
             $books->save();
             return view("/uploadbooks");
 
